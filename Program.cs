2c7c535cc1e750aba8bb1038aeb63bfae0982616
BoomZa Nightcore <krittameth193@gmail.com>
2021-03-28 05:33:07 +0700

    question 2's anwser

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace workshoptest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int x = a;
+            int y = b;
+
+            while (x != y)
+            {
+                if (x < y)
+                {
+                    x = x + a;
+                }
+                else
+                {
+                    y = y + b;
+                }
+            }
+            Console.WriteLine(x);
+        }
+    }
+}
